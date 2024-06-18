79d4999ddbec7a059bddd57fd44f6fa919bc3ed0
Biswajeet Sahoo <2000biswajeet@gmail.com>
2024-06-19 00:30:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+  const [users, setUser] = useState([])
+  const [pageNumber, setPageNumber] = useState(1)
+
+  useEffect(()=>{
+    fetchNextUser(pageNumber).then((data) => {
+      setUser((prevUsers)=>{
+        const newUsers = [...prevUsers, ...data]
+        const uniqueUsers = Array.from(new Set(newUsers.map(user => user.id)))
+                          .map(id => newUsers.find(user => user.id === id))
+        return uniqueUsers
+      })
+    })
+  }, [pageNumber])
+  console.log(users);
+
+  const fetchNextUser = async (pageNumberToken) => {
+    // const response = await fetch(`https://picsum.photos/v2/list?page=${pageNumberToken}&limit=10`)
+    // const fetchResponseData = await response.json()
+    // return fetchResponseData
+    const response =  await axios.get(`https://picsum.photos/v2/list?page=${pageNumberToken}&limit=10`)
+    return response.data
+  }
+
+  const loadMore = () => {
+    setPageNumber((prevPageNumber) => prevPageNumber+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gallery'>
+      {users.map((user) => 
+          <img 
+          key={user.id}
+          alt={user.author}
+          src={user.download_url}
+          height={400}
+          width={400}
+          />
+      )}
+
+      </div>
+      <button className='load' onClick={loadMore}>Load</button>
     </div>
   );
 }
